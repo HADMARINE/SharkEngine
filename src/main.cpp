f83aa38f6e43db0d7d962f1f10a70aaf7f5eebf2
HADMARINE <tsx@kakao.com>
2020-09-21 11:10:59 +0900

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,15 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+//
+// Created by HADMARINE on 2020/09/20.
+//
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-#include <vulkan/vulkan.h>
-
-#include "stdafx.hpp"
-#include <json/json.h>
-#include "Assets.hpp"
+#include "HadmarineEngine.hpp"
 
 int main() {
-  glfwInit();
-
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-  std::cout << extensionCount << " extensions supported\n";
-
-  glm::mat4 matrix;
-  glm::vec4 vec;
-  auto test = matrix * vec;
-
-  while(!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
+  HadmarineEngine engine;
+  try{
+    engine.run();
+  } catch (const std::exception& e) {
+    return EXIT_FAILURE;
   }
-
-  glfwDestroyWindow(window);
-
-  glfwTerminate();
-
-  return 0;
+  return EXIT_SUCCESS;
 }
