64a749385c09e2b3efa4b12662b951efc58dcc98
HADMARINE <tsx@kakao.com>
2020-09-24 20:15:55 +0900

    update global_preferences

--- src/global_preferences.hpp
+++ src/global_preferences.hpp
@@ -10,7 +10,9 @@
 namespace GlobalPreferences {
     constexpr const char * APPLICATION_NAME = "HADMARINE_ENGINE";
     constexpr const bool DEBUG = true;
+
     constexpr const uint32_t SCREEN_WIDTH = 800;
     constexpr const uint32_t SCREEN_HEIGHT = 600;
 
+    constexpr const int MAX_FRAMES_IN_FLIGHT = 2;
 }
