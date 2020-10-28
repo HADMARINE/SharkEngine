21e90041d4232e62b886d820a47fd9e4271381d6
HADMARINE <tsx@kakao.com>
2020-10-28 21:33:58 +0900

    Update coredefine

--- src/include/Engine/CoreDefine.h
+++ src/include/Engine/CoreDefine.h
@@ -6,35 +6,35 @@
 #define VULKAN_ENGINE_COREDEFINE_H
 
 #include "glm/glm.hpp"
+#include <array>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/quaternion.hpp>
 #include <glm/gtx/transform.hpp>
-#include <array>
 #include <queue>
 
 //DELETE Macro
-#define SAFE_DELETE(p)  \
-	{                   \
-		if (p) {        \
-			delete (p); \
-			(p) = 0;    \
-		}               \
-	}
+#define SAFE_DELETE(p)     \
+    {                      \
+        if (p) {           \
+            delete (p);    \
+            (p) = nullptr; \
+        }                  \
+    }
 
 #define SAFE_DELETE_ARRAY(p) \
-	{                        \
-		if (p) {             \
-			delete[](p);     \
-			(p) = 0;         \
-		}                    \
-	}
+    {                        \
+        if (p) {             \
+            delete[](p);     \
+            (p) = nullptr;   \
+        }                    \
+    }
 
 #define SAFE_RELEASE(p)     \
-	{                       \
-		if (p) {            \
-			(p)->Release(); \
-			(p) = 0;        \
-		}                   \
-	}
+    {                       \
+        if (p) {            \
+            (p)->Release(); \
+            (p) = nullptr;  \
+        }                   \
+    }
 
 #endif//VULKAN_ENGINE_COREDEFINE_H
