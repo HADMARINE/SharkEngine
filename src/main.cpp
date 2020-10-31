570f1296eeeac63d84d447cc22cc13f83cabe544
HADMARINE <tsx@kakao.com>
2020-10-31 11:08:59 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,22 @@
 // Created by HADMARINE on 2020/09/20.
 //
 
+#include "include/Engine/Audio/SharkAudio.hpp"
 #include "include/Graphic/VulkanApplication.h"
 
 std::vector<const char *> layerNames = {
-        "VK_LAYER_LUNARG_standard_validation"
-};
+        "VK_LAYER_LUNARG_standard_validation"};
 
 std::vector<const char *> deviceExtensionNames = {
-        VK_KHR_SWAPCHAIN_EXTENSION_NAME
-};
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
 int main() {
-    VulkanApplication* appObj = VulkanApplication::GetInstance();
+    SharkEngine::Audio::initialize();
+    SharkEngine::Audio::SourcePreferences audioPref;
+    SharkEngine::Audio audio("wavTester1.wav", audioPref);
+
+    audio.play();
+    VulkanApplication *appObj = VulkanApplication::GetInstance();
     appObj->Initialize();
     appObj->Prepare();
     bool isWindowOpen = true;
