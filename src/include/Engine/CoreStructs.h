7125baec7505048538932a54d804492ac5ccb85c
HADMARINE <tsx@kakao.com>
2020-11-01 17:14:11 +0900

    Update CoreStructs

--- src/include/Engine/CoreStructs.h
+++ src/include/Engine/CoreStructs.h
@@ -5,23 +5,48 @@
 #ifndef SHARKENGINE_CORESTRUCTS_H
 #define SHARKENGINE_CORESTRUCTS_H
 
-#include <vector>
 #include "Scene/Entity/Entity.h"
+#include <vector>
 
 #include <glm/glm.hpp>
 //#include <glm/gtc/matrix_transform.hpp>
 //#include <glm/gtx/quaternion.hpp>
 //#include <glm/gtx/transform.hpp>
 
-using namespace SharkEngine::Core;
-
 /*************** Collision **************/
-namespace {
+namespace SharkEngine::Core {
     struct Collision {
-        Entity* entity;
-        std::vector<glm::vec3> contacts;   //Contact points
+        Entity *entity;
+        std::vector<glm::vec3> contacts;//Contact points
     };
-}
+
+    namespace Physics {
+        template <typename T>
+        class RigidBody {
+        public:
+            T object;
+            glm::vec3 vel;
+            float mass;
+            float strength;
+        };
+
+        struct Circle {
+            float radius;
+            glm::vec3 pos;
+        };
+
+        struct AABB {
+            glm::vec3 pos;
+            glm::vec3 scale;
+        };
+
+        struct OBB {
+            glm::vec3 pos;
+            float rot;
+            glm::vec3 scale;
+        };
+    }// namespace Physics
+}// namespace SharkEngine::Core
 
 
 #endif//SHARKENGINE_CORESTRUCTS_H
