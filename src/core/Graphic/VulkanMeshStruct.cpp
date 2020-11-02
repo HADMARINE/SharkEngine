e1759d94b51ddd3edc2bd20ab653dab9f83ad923
eunwoosong <songe00348@gmail.com>
2020-11-03 02:27:02 +0900

    #0001 Roll back Shark Engine Graphic System
    Roll back

--- src/core/Graphic/VulkanMeshStruct.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by EunwooSong on 2020-10-28.
-//
-
-#include "../../include/Graphic/VulkanMeshStruct.h"
--- src/include/Graphic/IncludeVulkan.h
+++ src/include/Graphic/IncludeVulkan.h
@@ -1,40 +1,8 @@
 //
-// Created by EunwooSong on 2020-10-27.
+// Created by EunwooSong on 2020-11-03.
 //
 
 #ifndef SHARKENGINE_INCLUDEVULKAN_H
 #define SHARKENGINE_INCLUDEVULKAN_H
-
-
-#pragma warning(disable : 4819)
-
-#include "../Engine/CoreDefine.h"
-/*********** C/C++ HEADER FILES ***********/
-
-/*********** COMPILER SPECIFIC PREPROCESSORS ***********/
-#if defined(_WIN32) || defined(_WIN64)
-//#define WIN32_LEAN_AND_MEAN
-//#define NOMINMAX
-//#define VK_USE_PLATFORM_XCB_KHR
-#elif defined(__APPLE__)
-#define VK_USE_PLATFORM_MACOS_MVK
-#endif
-
-/*********** GLM HEADER FILES ***********/
-#define GLM_FORCE_RADIANS
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-/*********** GLI HEADER FILES ***********/
-#include <gli/gli.hpp>
-#include <gli/texture2d.hpp>
-
-/*********** VULKAN HEADER FILES ***********/
-#include <vulkan/vulkan.h>
-#include <GLFW/glfw3.h>
-
-#ifdef AUTO_COMPILE_GLSL_TO_SPV
-//#include "SPIRV/GlslangToSpv.h"
-#endif
 
 #endif//SHARKENGINE_INCLUDEVULKAN_H
