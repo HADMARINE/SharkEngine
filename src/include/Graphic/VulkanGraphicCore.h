78a977559317a95d156a09aeee19df381a95fc9a
eunwoosong <songe00348@gmail.com>
2020-11-04 11:56:08 +0900

    vkError

--- src/include/Graphic/VulkanGraphicCore.h
+++ src/include/Graphic/VulkanGraphicCore.h
@@ -913,27 +913,27 @@ namespace VulkanCore {
             endSingleTimeCommands(commandBuffer);
         }
 
-        //    void createVertexBuffer() {
-        //        VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
-        //
-        //        VkBuffer stagingBuffer;
-        //        VkDeviceMemory stagingBufferMemory;
-        //        createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-        //                     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-        //                     stagingBuffer, stagingBufferMemory);
-        //
-        //        void *data;
-        //        vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
-        //        memcpy(data, vertices.data(), (size_t) bufferSize);
-        //        vkUnmapMemory(device, stagingBufferMemory);
-        //
-        //        createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-        //                     VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
-        //
-        //        copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
-        //        vkDestroyBuffer(device, stagingBuffer, nullptr);
-        //        vkFreeMemory(device, stagingBufferMemory, nullptr);
-        //    }
+            void createVertexBuffer() {
+                VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
+
+                VkBuffer stagingBuffer;
+                VkDeviceMemory stagingBufferMemory;
+                createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                             stagingBuffer, stagingBufferMemory);
+
+                void *data;
+                vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+                memcpy(data, vertices.data(), (size_t) bufferSize);
+                vkUnmapMemory(device, stagingBufferMemory);
+
+                createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+                             VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
+
+                copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+                vkDestroyBuffer(device, stagingBuffer, nullptr);
+                vkFreeMemory(device, stagingBufferMemory, nullptr);
+            }
         //
         void createDescriptorSetLayout() {
             VkDescriptorSetLayoutBinding uboLayoutBinding{};
