293480e50b8eafd669231692338ef1f64cc79a24
HADMARINE <tsx@kakao.com>
2020-11-06 07:19:08 +0900

    Update LIB

--- src/core/Engine/SystemManager/ColliderManager.cpp
+++ src/core/Engine/SystemManager/ColliderManager.cpp
@@ -99,8 +99,8 @@ namespace SharkEngine::Core::Physics {
         setMinMax(A, a);
         setMinMax(B, b);
 
-        if (a.max.x < b.min.x or a.min.x > b.max.x) return false;
-        if (a.max.y < b.min.y or a.min.y > b.max.y) return false;
+        if (a.max.x < b.min.x || a.min.x > b.max.x) return false;
+        if (a.max.y < b.min.y || a.min.y > b.max.y) return false;
 
         return true;
     }
